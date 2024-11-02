664e5b7f313cde61eaf489d0d7d694ef78617adc
Caio Felipe Fernandes de Andrade <cartiFelipe05@gmail.com>
2024-11-02 17:38:55 -0300

    update basico

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,9 +1,14 @@
+import { useState } from "react";
 import ButtonComponent from "../components/button/Index";
 
-const Home = () => (
-  <div>
-    <h1>Bottão material ui</h1>
-    <ButtonComponent props={undefined}>Clique em mim</ButtonComponent>
-  </div>
-);
+const Home = () => {
+  const [label, setLabel] = useState("Clique em mim");
+
+  return (
+    <div>
+      <h1>Bottão material ui</h1>
+      <ButtonComponent props={undefined}>{label}</ButtonComponent>
+    </div>
+  );
+};
 export default Home;
