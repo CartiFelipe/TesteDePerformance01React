d254a0cef3119504caf76b7f10f444b5b9fbd53e
Caio Felipe Fernandes de Andrade <cartiFelipe05@gmail.com>
2024-11-02 17:36:32 -0300

    Rotas e botão material ui criados

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,2 +1,9 @@
-const Home = () => "home";
+import ButtonComponent from "../components/button/Index";
+
+const Home = () => (
+  <div>
+    <h1>Bottão material ui</h1>
+    <ButtonComponent props={undefined}>Clique em mim</ButtonComponent>
+  </div>
+);
 export default Home;
